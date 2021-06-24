3e1e1dbc21a5bbd4c15cf13750cd8129ff6b197e
Lucas Jaroszewski <ljnunes@outlook.com.br>
2021-06-24 10:29:34 -0300

    Created user login view

--- users/views.py
+++ users/views.py
@@ -1,10 +1,35 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 from .serializers import UserSerializer
+from .models import User
 
 class RegisterView(APIView):
     def post(self, request):
+
+        # Serializes the data
         serializer = UserSerializer(data=request.data)
+
+        # Validates the serializer
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        # Finds the user
+        user = User.objects.filter(email=email).first()
+
+        # Validate the user
+        if user is None:
+            raise AuthenticationFailed('User not found.')
+
+        # Compare the password and validate
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password.')
+
+        return Response({"message": "success"})
