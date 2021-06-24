79be308b2a9fa99bcdc0522dd96464cbc18380f6
Lucas Jaroszewski <ljnunes@outlook.com.br>
2021-06-24 10:49:12 -0300

    Added JWT configuration to user login view

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from .serializers import UserSerializer
 from .models import User
+import jwt, datetime
 
 class RegisterView(APIView):
     def post(self, request):
@@ -32,4 +33,21 @@ class LoginView(APIView):
         if not user.check_password(password):
             raise AuthenticationFailed('Incorrect password.')
 
-        return Response({"message": "success"})
+        # Stores the data the user will use with an expiration date of 60 minutes
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        # Creates the token with payload and secret
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        # Return token via cookies
+        response = Response()
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt': token
+        }
+
+        return response
