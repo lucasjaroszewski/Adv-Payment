2127c269ce2a4b155e09955fea0cf8fe94a8e4a0
Lucas Jaroszewski <ljnunes@outlook.com.br>
2021-06-23 22:29:41 -0300

    Fixed inconsistent migration exception

--- advance_payment/settings.py
+++ advance_payment/settings.py
@@ -44,7 +44,7 @@ ALLOWED_HOSTS = ['adv-payment-jwt.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    #'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
