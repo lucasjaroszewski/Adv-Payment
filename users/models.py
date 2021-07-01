1f2356dc6027b2fcdc8b27c1605be62859ca8bf4
Lucas Jaroszewski <ljnunes@outlook.com.br>
2021-07-01 19:29:15 -0300

    Updated Payment model.

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import datetime as dt
 
 class User(AbstractUser):
     name = models.CharField(max_length=255)
@@ -9,3 +10,22 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+class Payment(models.Model):
+    # User authentication
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='payments')
+
+    # Description
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    date_added = models.DateField(auto_now_add=True)
+    expiration_date = models.DateField()
+
+    def save(self, *args, **kwargs):
+        super(Payment, self).save(*args, **kwargs)
+
+    def days_left(self):
+        return (self.expiration_date - dt.date.today()).days
+
+    def __str__(self):
+        return f'User: {self.user} ({self.user.id}) - {self.name}'
